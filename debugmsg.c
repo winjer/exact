6b862e09b1344deb8a6b2b84e507cbf3c0e84184
Doug Winter <doug.winter@isotoma.com>
2003-01-23 14:18:42 +0000

    check

--- debugmsg.c
+++ debugmsg.c
@@ -1,21 +1,27 @@
-/* $Id: debugmsg.c,v 1.3 2003/01/23 12:34:43 doug Exp $
+/* $Id: debugmsg.c,v 1.4 2003/01/23 14:18:42 doug Exp $
 */
 
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <syslog.h>
 
 #include "config.h"
 #include "conffile.h"
+
+void debug_init() {
+	openlog("exact", 0, LOG_MAIL);
+}
 
 void debugmsg(int level, char *fmt, ...) {
 #ifdef HAVE_VPRINTF
 	if(level<=conffile_param_int("loglevel")) {
 		va_list ap;
 		va_start(ap,fmt);
-		vfprintf(stderr, fmt, ap);
+		vsyslog(stderr, fmt, ap);
 		fflush(stderr);
 		va_end(ap);
 	}
 #endif
 }
+
